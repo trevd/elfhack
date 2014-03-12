6e56755be6ef87226e89669e7d0c5f94530eec34
android <trevd1234@gmail.com>
2014-03-12 15:35:21 +0000

    update Android.mk

--- libelf_examples/Android.mk
+++ libelf_examples/Android.mk
@@ -6,8 +6,11 @@ libelf_examples_common_cflags := \
 	-Wno-unused-parameter \
 
 libelf_examples_common_includes := \
-	$(ELFUTILS_PATH)/libelf
+	$(ELFUTILS_PATH)/libelf \
+	$(ELFUTILS_PATH)/0.153/libelf
 
+
+#========= get-elf-type ============
 
 include $(CLEAR_VARS)
 LOCAL_CLANG := true
@@ -25,4 +28,24 @@ LOCAL_SRC_FILES := get-elf-type.c
 LOCAL_C_INCLUDES := $(libelf_examples_common_includes) 
 LOCAL_STATIC_LIBRARIES := libelf
 LOCAL_MODULE := get-elf-type
+include $(BUILD_HOST_EXECUTABLE)
+
+#========= print-elf-header ============
+
+include $(CLEAR_VARS)
+LOCAL_CLANG := true
+LOCAL_CFLAGS := $(libelf_examples_common_cflags)
+LOCAL_SRC_FILES := print-elf-header.c
+LOCAL_C_INCLUDES := $(libelf_examples_common_includes) 
+LOCAL_STATIC_LIBRARIES := libelf
+LOCAL_MODULE := print-elf-header
+include $(BUILD_EXECUTABLE)
+
+
+include $(CLEAR_VARS)
+LOCAL_CFLAGS := -D_GNU_SOURCE $(libelf_examples_common_cflags)
+LOCAL_SRC_FILES := print-elf-header.c
+LOCAL_C_INCLUDES := $(libelf_examples_common_includes) 
+LOCAL_STATIC_LIBRARIES := libelf
+LOCAL_MODULE := print-elf-header
 include $(BUILD_HOST_EXECUTABLE)
